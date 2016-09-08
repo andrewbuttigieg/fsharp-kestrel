e0773c61364b12dbdc49809f7cf18cc16f2d2696
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2016-09-08 14:23:01 +0200

    Made hot port configurable.

--- Program.fs
+++ Program.fs
@@ -1,18 +1,26 @@
-﻿open System
-open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.Http
+﻿namespace KestrelFSharp
+
+module Main = 
+    open System
+    open Microsoft.AspNetCore.Hosting
+    open Microsoft.AspNetCore.Builder
+    open Microsoft.AspNetCore.Hosting
+    open Microsoft.AspNetCore.Http
+    open Microsoft.Extensions.Configuration
 
 
-type Startup() = 
-    member this.Configure(app: IApplicationBuilder) =
-      app.Run(fun context -> context.Response.WriteAsync("Hello from ASP.NET Core!"))
+    type Startup() = 
+        member this.Configure(app: IApplicationBuilder) =
+        app.Run(fun context -> context.Response.WriteAsync("Hello from ASP.NET Core!"))
 
+    let configuration = (new ConfigurationBuilder())
+                            .AddInMemoryCollection(dict["server.urls","http://localhost:8090";])
+                            .AddEnvironmentVariables()
+                            .Build()
 
-[<EntryPoint>]
-let main argv = 
-    let host = WebHostBuilder().UseKestrel().UseStartup<Startup>().Build()
-    host.Run()
-    printfn "Server finished!"
-    0
+    [<EntryPoint>]
+    let main argv = 
+        let host = WebHostBuilder().UseKestrel().UseConfiguration(configuration).UseStartup<Startup>().Build()
+        host.Run()
+        printfn "Server finished!"
+        0
